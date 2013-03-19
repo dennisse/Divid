4ab8132845e96fb80cd118b3a5423e07e63af331
Dennis Eriksen <dennis.se@gmail.com>
2013-03-19 06:20:59 -0400

    Added some comments

--- app.js
+++ app.js
@@ -9,26 +9,49 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
-var app = express();
+var app = express(); // initiates express
+
+
+
+/**
+ * App configuration
+ */
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+    // this controls the port the application will be running on.
+    // by adding 'process.enc.PORT' we enable the app to run on automated systems like heroku
+    app.set('port', process.env.PORT || 8000);
+
+    app.set('views', __dirname + '/views'); // sets views to the right directory
+    app.set('view engine', 'ejs'); // initiates viewengine. We use EJS, or embedded js - http://embeddedjs.com/
+    app.use(express.favicon()); // sets favicon
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+
+/**
+ * Routes
+ */
+
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+
+
+
+
+
+/**
+ * Server initiation
+ */
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
