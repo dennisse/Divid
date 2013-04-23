8358e0cea44ddc8830694de6b58e307d516cffcb
robertkj <joramo40@hotmail.com>
2013-04-23 11:34:16 +0200

    fixed home page

--- router.js
+++ router.js
@@ -30,6 +30,13 @@ module.exports = function(app) {
         });
     });
 
+    app.get('/home', function(req, res) {
+        res.render('home', {
+            title: 'home',
+            loggedin: false
+        });
+    });
+
 
     /*
      * GET dashboard
