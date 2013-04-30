c0630c6db730ba50ab247435bff14a36a4303e93
Dennis Eriksen <dennis.se@gmail.com>
2013-04-30 13:28:45 +0200

    added shorturl for projects

--- models/project.js
+++ models/project.js
@@ -13,7 +13,8 @@ var ProjectSchema = new Schema({
     currency: { type: String, default: 'kr', trim: true },
     public: { type: String, default: 'invite-only' },
     created: { type: Date, default: Date.now },
-    updated: { type: Date, default: Date.now }
+    updated: { type: Date, default: Date.now },
+    shortURL: { type: String, unique: true }
 });
 
 // the four validations below only apply if you are signing up traditionally
@@ -22,6 +23,21 @@ ProjectSchema.path('name').validate(function(name) {
     // if you're authenticated by any of the oauth strategies (facebook, twitter), don't validate
     return name.length;
 }, 'Project name cannot be blank');
+
+
+ProjectSchema.pre('save', function(next) {
+    if (this.shortURL !== undefined) return next();
+    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    this.shortURL = '';
+    for (var i = 0; i < 6; i++) {
+        var x = Math.floor(Math.random() * chars.length);
+        this.shortURL += chars.charAt(x);
+    }
+    console.log('SHORT: ' + this.shortURL);
+    next();
+});
+
+
 
 ProjectSchema.statics = {
 
