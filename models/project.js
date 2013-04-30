334cdccec27afcb1a353df49f7edb77181048843
Dennis Eriksen <dennis.se@gmail.com>
2013-04-30 12:25:44 +0200

    made a function for returning project by ID

--- models/project.js
+++ models/project.js
@@ -6,12 +6,8 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
 
-
-
-
-
 var ProjectSchema = new Schema({
-    user: { type: String, ref: 'User' },
+    user: { type: Schema.ObjectId, ref: 'User' },
     name: { type: String, default: '', trim: true },
     description: {type: String, default: '', trim: true },
     currency: { type: String, default: 'kr', trim: true },
@@ -20,7 +16,6 @@ var ProjectSchema = new Schema({
     updated: { type: Date, default: Date.now }
 });
 
-
 // the four validations below only apply if you are signing up traditionally
 
 ProjectSchema.path('name').validate(function(name) {
@@ -28,11 +23,28 @@ ProjectSchema.path('name').validate(function(name) {
     return name.length;
 }, 'Project name cannot be blank');
 
-ProjectSchema.methods = {
+ProjectSchema.statics = {
 
     log: function() {
         console.log('wat. wat logged this');
+    },
+
+
+
+   /**
+    * Find project by id
+    *
+    * @param {ObjectId} id
+    * @param {Function} callback
+    * @api private
+    */
+
+    load: function(id, callback) {
+        this.findOne({ _id: id })
+          .populate('user')
+          .exec(callback);
     }
+
 
 }
 
