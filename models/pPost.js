e7cf0075829e1424d5c2fcbeeac97509b404b856
Dennis Eriksen <dennis.se@gmail.com>
2013-05-09 10:30:53 +0200

    changed the model

--- models/pPost.js
+++ models/pPost.js
@@ -6,49 +6,32 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
 
-var ProjectSchema = new Schema({
+var pPostSchema = new Schema({
     user: { type: Schema.ObjectId, ref: 'User' },
-    name: { type: String, default: '', trim: true },
-    description: {type: String, default: '', trim: true },
+    for: { type: Schema.ObjectId, ref: 'User' },
+    project: { type: Schema.ObjectId, ref: 'Project' },
+    what: { type: String, default: '', trim: true },
+    comment: {type: String, default: '', trim: true },
+    participants: [],
+    value: { type: Number, defailt: 0 },
+    file: { type: String, default: '', trim: true },
     currency: { type: String, default: 'kr', trim: true },
-    public: { type: String, default: 'invite-only' },
     created: { type: Date, default: Date.now },
-    updated: { type: Date, default: Date.now },
-    shortURL: { type: String, unique: true }
-});
-
-// the four validations below only apply if you are signing up traditionally
-
-ProjectSchema.path('name').validate(function(name) {
-    // if you're authenticated by any of the oauth strategies (facebook, twitter), don't validate
-    return name.length;
-}, 'Project name cannot be blank');
-
-
-ProjectSchema.pre('save', function(next) {
-    if (this.shortURL !== undefined) return next();
-    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    this.shortURL = '';
-    for (var i = 0; i < 6; i++) {
-        var x = Math.floor(Math.random() * chars.length);
-        this.shortURL += chars.charAt(x);
-    }
-    console.log('SHORT: ' + this.shortURL);
-    next();
+    updated: { type: Date, default: Date.now }
 });
 
 
-
-ProjectSchema.statics = {
-
-    log: function() {
-        console.log('wat. wat logged this');
-    },
+pPostSchema.path('what').validate(function(name) {
+    return what.length;
+}, 'You need to name the expense');
 
 
+
+
+pPostSchema.statics = {
 
    /**
-    * Find project by id
+    * Find post by id
     *
     * @param {ObjectId} id
     * @param {Function} callback
@@ -64,4 +47,4 @@ ProjectSchema.statics = {
 
 }
 
-mongoose.model('Project', ProjectSchema);
+mongoose.model('pPost', pPostSchema);
